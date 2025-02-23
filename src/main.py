fb3d30bf8bf8e4fb37f42682daa4e2c075595b7e
HLA WIN TUN (Ko Ye) <66273305+koye744@users.noreply.github.com>
2025-02-22 21:23:04 -0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# main.py - Simple Python Quiz App
+print("Welcome to CodeQuest: Python Mastery!")
+
+answer = input("What is the output of print(2 + 2)? ")
+if answer == "4":
+    print("Correct!")
+else:
+    print("Try again!")
